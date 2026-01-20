c1b072715dc380c308848cb75635b1ef86e3c800
Anushabanoth <banothanusha900@gmail.com>
2026-01-20 23:21:24 +0530

    Delete avi_test_framework directory

--- avi_test_framework/framework/README.md
+++
@@ -1,8 +0,0 @@
-# Logs Directory
-
-This directory contains test execution logs.
-
-## File Naming Convention
-- `test_execution_YYYYMMDD_HHMMSS.log` - Timestamped log files
-
-## Log Format
--- avi_test_framework/framework/validators.py
+++
@@ -1,8 +0,0 @@
-import logging
-
-logger = logging.getLogger(__name__)
-
-def validate_response(response: dict, expected_keys: list) -> bool:
-    if not response:
-        return False
-    return all(key in response for key in expected_keys)
